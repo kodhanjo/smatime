9a579307d5dcb924a95c6cf1ec2f7f90857ed6f1
Koech-code <nixon.koech@moringaschool.com>
2021-08-24 16:32:30 +0300

    created manage file

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
-from app import app
-
+from app import create_app,db
+from flask_script import Manager, Server
+from app.models import User
 
 app = create_app('development')
+manager = Manager(app)
+manager.add_command('server', Server)
 
-manager= Manager(app)
-manager.add_command('server',Server)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+
+    
 if __name__ == '__main__':
-    manager.run()  
+    manager.run()
+
