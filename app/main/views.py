4a511c3e10700177a7a896a544c97f1a337a6502
kodhanjo <felix.osambo@student.moringaschool.com>
2021-08-25 16:01:35 +0300

    added images in my static folder

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,16 @@ from . import main
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    items= [
+        {'ID': 1, "image": '#', 'name': 'blackhair', 'price': 50},
+        {'ID': 2, "image": '#', 'name': 'Box', 'price': 90},
+        {'ID': 3, "image": '#', 'name': 'beards', 'price': 70},
+        {'ID': 4, "image": '#', 'name': 'dreadlocks', 'price': 55},
+        {'ID': 5, "image": '#', 'name': 'banana', 'price': 65},
+        {'ID': 6, "image": '#', 'name': 'Facialhair', 'price': 75},
+        {'ID': 7, "image": '#', 'name': 'buzzcut', 'price': 80},
+        {'ID': 8, "image": '#', 'name': 'fade', 'price': 120}
+    ]
+    return render_template('index.html', items=items)
 
-@main
+
